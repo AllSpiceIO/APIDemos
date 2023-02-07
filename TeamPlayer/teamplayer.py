3a20736f909496dea5449f48fe16b3e9f1f913e7
Daniel Lindmark <daniel@allspice.io>
2023-02-06 18:05:04 -0600

    Add new allspice/py-gitea wrapper

--- TeamPlayer/teamplayer.py
+++ TeamPlayer/teamplayer.py
@@ -1,40 +1,21 @@
-# Based on https://github.com/Langenfeld/py-gitea
+# teamplayer.py
 
+# script to demonstrate organization / team / member / permissions
 
-
+# Load the allspice/gitea module
 import os
-# from gitea import *
+import sys
+sys.path.append('..')
+from allspice_proto.allspice_proto import AllSpice_Proto
+allspice = AllSpice_Proto()
+allspice.start()
 
-# Importing locally from /gitea/gitea/, not the installed py-gitea api
-from gitea.gitea import *
 
 # helper class, different than gitea.teams, should merge
 from src.team import Team
 
-# Load URL and access token from environmental variables
-try:
-    URL = os.environ['ALLSPICE_URL']
-    # URL = "https://allspice.dev"
-except:
-    print('ALLSPICE_URL is None, run >export ALLSPICE_URL="https://allspice.dev"')
-    quit()
-
-
-try:
-    TOKEN = os.environ['ALLSPICE_ACCESS_TOKEN']
-except:
-    print (f'ALLSPICE_ACCESS_TOKEN is None, run >export ALLSPICE_ACCESS_TOKEN="token", create token at {URL}/user/settings/applications')
-    quit()
-
-
-# create website object  
-gitea = Gitea(URL, TOKEN)
-
-# -----------------------------------------------------
-# Setup
-
 # Create list of organizations to modify
-orgsToMod = ["UtilityTesting", "ExampleOrganization"]
+orgsToMod = ["UtilityTesting"]
 
 
 # Create teams
@@ -42,45 +23,45 @@ readWrite = "write"
 canCreateOrgRepo = True
 includesAllRepos = True
 teamList = []
-teamList.append( Team("Owners",
-                        "Owners of the org",
-                        readWrite,
-                        canCreateOrgRepo,
-                        includesAllRepos,
-                        {
-                            "repo.code": "write",
-                            "repo.ext_issues": "write",
-                            "repo.ext_wiki": "write",
-                            "repo.issues": "write",
-                            "repo.projects": "write",
-                            "repo.pulls": "write",
-                            "repo.releases": "write",
-                            "repo.wiki": "write"
-                        }, 
-                        ["daniel@allspice.io", "daniel+test2@allspice.io"]))
+# teamList.append( Team("Owners",
+#                         "Owners of the org",
+#                         readWrite,
+#                         canCreateOrgRepo,
+#                         includesAllRepos,
+#                         {
+#                             "repo.code": "write",
+#                             "repo.ext_issues": "write",
+#                             "repo.ext_wiki": "write",
+#                             "repo.issues": "write",
+#                             "repo.projects": "write",
+#                             "repo.pulls": "write",
+#                             "repo.releases": "write",
+#                             "repo.wiki": "write"
+#                         }, 
+#                         ["daniel@allspice.io", "daniel+test2@allspice.io"]))
 
-canCreateOrgRepo = False
-readWrite = "read"
-teamList.append( Team("Collaborators", 
-                        "Can read and participate in design reviews",
-                        readWrite,
-                        canCreateOrgRepo,
-                        includesAllRepos,
-                        {
-                            "repo.code": "read",
-                            "repo.ext_issues": "none",
-                            "repo.ext_wiki": "none",
-                            "repo.issues": "read",
-                            "repo.projects": "read",
-                            "repo.pulls": "read",
-                            "repo.releases": "read",
-                            "repo.wiki": "read"
-                        }, 
-                        ["daniel+revareviewa@allspice.io", "daniel+mikachanical@allspice.io"]))
+# canCreateOrgRepo = False
+# readWrite = "read"
+# teamList.append( Team("Plumbers", 
+#                         "Can read and participate in design reviews",
+#                         readWrite,
+#                         canCreateOrgRepo,
+#                         includesAllRepos,
+#                         {
+#                             "repo.code": "read",
+#                             "repo.ext_issues": "none",
+#                             "repo.ext_wiki": "none",
+#                             "repo.issues": "read",
+#                             "repo.projects": "read",
+#                             "repo.pulls": "read",
+#                             "repo.releases": "read",
+#                             "repo.wiki": "read"
+#                         }, 
+#                         ["daniel+revareviewa@allspice.io", "daniel+mikachanical@allspice.io"]))
 
 canCreateOrgRepo = True
 readWrite = "write"
-teamList.append( Team("Contributors", 
+teamList.append( Team("ChupChups", 
                         "write acceess to team repos",
                         readWrite,
                         canCreateOrgRepo,
@@ -92,7 +73,7 @@ teamList.append( Team("Contributors",
                             "repo.issues": "write",
                             "repo.projects": "write",
                             "repo.pulls": "write",
-                            "repo.releases": "write",
+                            "repo.releases": "read",
                             "repo.wiki": "write"
                         }, 
                         ["daniel@allspice.io", "daniel+mikachanical@allspice.io"]))
@@ -112,30 +93,47 @@ units=(
 # --------------------------------------------------
 # Start script
 
-print("TeamPlayer python script")
-print("Gitea Version: " + gitea.get_version())
-print("API-Token belongs to user: " + gitea.get_user().username)
+DEBUG_EMAILS = False
+DEBUG_TEAMS  = False
+DEBUG_ORG    = False
+
+
+
+hub = allspice.hub
+allspice.log.info(f'TeamPlayer python script')
+
 
 
 # get all organization names
-organizations = gitea.get_orgs()
+organizations = hub.get_orgs()
 
 # process all organizations on site
 for organization in organizations:
 
     if organization.name in orgsToMod:
-        print(f'Modifying organization, {organization.name}')
+        if DEBUG_ORG:
+            allspice.log.info(f'Modifying organization, {organization.name}')
 
         for team in teamList:
             units_map = team.units_map
             teamToMod = organization.get_team(team.name)
-            print(f'Modifying organization: {organization.name}, adding team: {team.name}')
+            
             if (teamToMod == None):
-                teamToMod = gitea.create_team(organization, team.name, team.description, team.readWrite, team.canCreateOrgRepo, team.includesAllRepos, units, units_map)            
+                teamToMod = hub.create_team(organization, team.name, team.description, team.readWrite, team.canCreateOrgRepo, team.includesAllRepos, units, units_map)            
+                if (DEBUG_TEAMS):
+                    allspice.log.info(f'Modifying organization: {organization.name}, adding team: {team.name}')
+            else:
+                if (DEBUG_TEAMS):
+                    allspice.log.info(f'Modifying organization: {organization.name}, team already exists: {team.name}')
+
 
             # Add users to team
+
             for email in team.memberEmails:
-                user = gitea.get_user_by_email(email)
+                user = hub.get_user_by_email(email)
+                if DEBUG_EMAILS:
+                    allspice.log.info(f'Modifying organization: {organization.name}, team {team.name}, adding email={email}')
+                
                 if teamToMod is not None:
                     teamToMod.add_user(user)
 
