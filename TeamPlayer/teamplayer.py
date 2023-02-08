ee68b5bd1766e2ba2082833f7a7e8b102a097327
Daniel Lindmark <daniel@allspice.io>
2023-02-08 16:31:08 -0600

    Fix indent bug

--- TeamPlayer/teamplayer.py
+++ TeamPlayer/teamplayer.py
@@ -12,7 +12,7 @@ allspice.start()
 
 
 # helper class, different than gitea.teams, should merge
-from src.team import Team
+from allspice_proto.src.team import Team
 
 # Create list of organizations to modify
 orgsToMod = ["UtilityTesting"]
@@ -62,7 +62,7 @@ teamList = []
 canCreateOrgRepo = True
 readWrite = "write"
 teamList.append( Team("ChupChups", 
-                        "write acceess to team repos",
+                        "update description",
                         readWrite,
                         canCreateOrgRepo,
                         includesAllRepos,
@@ -93,44 +93,74 @@ units=(
 # --------------------------------------------------
 # Start script
 
-DEBUG_EMAILS = False
-DEBUG_TEAMS  = False
-DEBUG_ORG    = False
+DEBUG_EMAILS   = False
+DEBUG_ORG_LIST = False
+DEBUG_ORG      = False
+DEBUG_TEAMS    = True
 
 
-
-hub = allspice.hub
+# hub = allspice.hub
 allspice.log.info(f'TeamPlayer python script')
 
 
 
 # get all organization names
-organizations = hub.get_orgs()
+organizations = allspice.hub.get_orgs()
+if DEBUG_ORG_LIST:
+    allspice.log.info(f'organization list')
+    for organization in organizations:
+        allspice.log.info(f'organization = {organization.name}')
 
 # process all organizations on site
 for organization in organizations:
 
     if organization.name in orgsToMod:
         if DEBUG_ORG:
-            allspice.log.info(f'Modifying organization, {organization.name}')
+            allspice.log.info(f'Modifying organization = {organization.name}')
+
+        teamnames = [] 
+        for team in teamList:
+            teamnames.append(team.name)
+
+        allspice.log.info(f'teamnames={teamnames}')
+        orgteams = organization.get_teams()
+        
+        DEBUG_ORG_TEAMS = True
+        for team in orgteams:
+            if DEBUG_ORG_TEAMS:
+                allspice.log.info(f'org = {organization.name}, Team = {team.name}, Id = {team.id}')
 
         for team in teamList:
             units_map = team.units_map
             teamToMod = organization.get_team(team.name)
             
+            # If the team doesn't exist create_team
+            # else: modify team
             if (teamToMod == None):
-                teamToMod = hub.create_team(organization, team.name, team.description, team.readWrite, team.canCreateOrgRepo, team.includesAllRepos, units, units_map)            
+                teamToMod = allspice.hub.create_team(   organization, 
+                                                        team.name, 
+                                                        team.description,
+                                                        team.readWrite,
+                                                        team.canCreateOrgRepo,
+                                                        team.includesAllRepos, 
+                                                        units, 
+                                                        units_map)            
                 if (DEBUG_TEAMS):
-                    allspice.log.info(f'Modifying organization: {organization.name}, adding team: {team.name}')
+                    allspice.log.info(f'Modifying organization = {organization.name}, creating team = {team.name}')
             else:
-                if (DEBUG_TEAMS):
-                    allspice.log.info(f'Modifying organization: {organization.name}, team already exists: {team.name}')
+                    
+                params = { 
+                    'description' : team.description,
+                    'name'        : team.name,
+                }
+
+                foo = allspice.hub.modify_team(teamToMod.id, params)
+                allspice.log.info(f'Modifying organization = {organization.name}, modifying team = {team.name}')
 
 
             # Add users to team
-
             for email in team.memberEmails:
-                user = hub.get_user_by_email(email)
+                user = allspice.hub.get_user_by_email(email)
                 if DEBUG_EMAILS:
                     allspice.log.info(f'Modifying organization: {organization.name}, team {team.name}, adding email={email}')
                 
