c81ef44fe14a2e59dd3f6e07c7e3a2c878b29413
Daniel Lindmark <daniel@allspice.io>
2023-02-08 20:43:05 -0600

    Create first proto of changeteamname.py

--- ChangeTeamName/changeteamname.py
+++ ChangeTeamName/changeteamname.py
@@ -60,48 +60,6 @@ if (    teamsToMod                  is None or
     allspice.log.error(f'Can\'t parse filename {args.filename}', teamsToMod={teamsToMod})
     quit()
 
-
-
-# canCreateOrgRepo = True
-# readWrite = "write"
-# teamList.append( Team("ChupChups", 
-#                         "update description",
-#                         readWrite,
-#                         canCreateOrgRepo,
-#                         includesAllRepos,
-#                         {
-#                             "repo.code": "write",
-#                             "repo.ext_issues": "write",
-#                             "repo.ext_wiki": "write",
-#                             "repo.issues": "write",
-#                             "repo.projects": "write",
-#                             "repo.pulls": "write",
-#                             "repo.releases": "read",
-#                             "repo.wiki": "write"
-#                         }, 
-#                         ["daniel@allspice.io", "daniel+mikachanical@allspice.io"]))
-
-# # permission units, associated with units_map
-# units=(
-#             "repo.code",
-#             "repo.issues",
-#             "repo.ext_issues",
-#             "repo.wiki",
-#             "repo.pulls",
-#             "repo.releases",
-#             "repo.ext_wiki",
-#     )
-
-
-# # --------------------------------------------------
-# # Start script
-
-# DEBUG_EMAILS   = False
-# DEBUG_ORG_LIST = False
-# DEBUG_ORG      = False
-# DEBUG_TEAMS    = True
-
-
 allspice.log.info(f'{__file__}, {allspice.URL} python script')
 
 orgs = allspice.hub.get_orgs();
@@ -136,94 +94,45 @@ for teamMods in teamsToMod['teamsToModify']:
         quit()
     
     # get organization's teams and check to see if oldTeamName is a team
+    newTeamName = teamMods['newTeamName']
     thisorg = None
     theseTeams = None
-    teamOnServer = False
+    newTeamOnServer = False
+    oldTeamOnServer = False
     oldTeamname = teamMods['oldTeamName']
+    teamID = 0
     try:
         thisorg = orgDict[orgname]
         theseTeams = thisorg.get_teams()
         for thisTeam in theseTeams:
-            print(f'-------->{thisorg.name}.{thisTeam.name}')
-            if thisTeam.name is oldTeamname :
-                teamOnServer = True
-                break;
+            if thisTeam.name == oldTeamname:
+                oldTeamOnServer = True
+                teamID = thisTeam.id
+            if thisTeam.name == newTeamName:
+                newTeamOnServer = True
     except:
         allspice.log.error(f'error {orgname}.getTeams()')
         quit()
 
-    if teamOnServer is False:
-        allspice.log.error(f'error {orgname}.getTeams(), teamname = {oldTeamname} not on server')
-        
+    # Check for errorsd
+    if  (   oldTeamOnServer is False or 
+            newTeamOnServer is True or
+            teamID == 0 or
+            newTeamName is None
     
-    
-    #print(orgs)
+    ):
+        allspice.log.error(f'error {orgname}.getTeams(), oldTeamname = {oldTeamname} on server = {oldTeamOnServer}, newTeamname = {newTeamName} on server = {newTeamOnServer} ')
+        quit()
 
-    # Check for current team
-    # allspice.hub.get
+    # setup https post parameters to modify team
+    params = { 'name' : newTeamName }
 
+    # Tell the server we want to modify the team with the new name
+    try:
+       
+        response = allspice.hub.modify_team(teamID, params)
+        allspice.log.info(f'Modifying organization = {orgname}, modifying team = {oldTeamname}, to {newTeamName}')
+    except:
+        allspice.log.error(f'error with allspice.hub.modifyTeam({teamID}), Modifying organization = {orgname}, modifying team = {oldTeamname}, to {newTeamName}')
 
-
-# # get all organization names
-# organizations = allspice.hub.get_orgs()
-# if DEBUG_ORG_LIST:
-#     allspice.log.info(f'organization list')
-#     for organization in organizations:
-#         allspice.log.info(f'organization = {organization.name}')
-
-# # process all organizations on site
-# for organization in organizations:
-
-#     if organization.name in orgsToMod:
-#         if DEBUG_ORG:
-#             allspice.log.info(f'Modifying organization = {organization.name}')
-
-#         teamnames = [] 
-#         for team in teamList:
-#             teamnames.append(team.name)
-
-#         allspice.log.info(f'teamnames={teamnames}')
-#         orgteams = organization.get_teams()
-        
-#         DEBUG_ORG_TEAMS = True
-#         for team in orgteams:
-#             if DEBUG_ORG_TEAMS:
-#                 allspice.log.info(f'org = {organization.name}, Team = {team.name}, Id = {team.id}')
-
-#         for team in teamList:
-#             units_map = team.units_map
-#             teamToMod = organization.get_team(team.name)
-            
-#             # If the team doesn't exist create_team
-#             # else: modify team
-#             if (teamToMod == None):
-#                 teamToMod = allspice.hub.create_team(   organization, 
-#                                                         team.name, 
-#                                                         team.description,
-#                                                         team.readWrite,
-#                                                         team.canCreateOrgRepo,
-#                                                         team.includesAllRepos, 
-#                                                         units, 
-#                                                         units_map)            
-#                 if (DEBUG_TEAMS):
-#                     allspice.log.info(f'Modifying organization = {organization.name}, creating team = {team.name}')
-#             else:
-                    
-#                 params = { 
-#                     'description' : team.description,
-#                     'name'        : team.name,
-#                 }
-
-#                 foo = allspice.hub.modify_team(teamToMod.id, params)
-#                 allspice.log.info(f'Modifying organization = {organization.name}, modifying team = {team.name}')
-
-
-#             # Add users to team
-#             for email in team.memberEmails:
-#                 user = allspice.hub.get_user_by_email(email)
-#                 if DEBUG_EMAILS:
-#                     allspice.log.info(f'Modifying organization: {organization.name}, team {team.name}, adding email={email}')
-                
-#                 if teamToMod is not None:
-#                     teamToMod.add_user(user)
-
+allspice.log.info(f'{__file__} completed')
