b950108eec08631f6ce2d69f7832d3906e91c588
Daniel Lindmark <daniel@allspice.io>
2023-02-18 16:38:09 -0600

    remove old readme text

--- assembly/assembly.py
+++ assembly/assembly.py
@@ -1,32 +1,13 @@
-# HelloWorld.py
-
-# If you're new to scripting, this is a great place to start. 
-
-# Hello World starts you out easy with some simple server requests.
-# This will help you troubleshoot your connection and show you the basics of making an api request
-
-import sys
-sys.path.append('..')
-from allspice.allspice import AllSpice
-allspice = AllSpice()
-allspice.start()
-
-versionResponse = ""
-try:
-    versionResponse = allspice.hub.get_version()
-except:
-    allspice.log.error(f'failure to gitea.get_version()')
-    quit()
-
-allspice.log.info(f'Allspice Version: {versionResponse}')
-
-username = ""
-try:
-    username = allspice.hub.get_user().username
-except:
-    allspice.log.error('User authentication invalid')
-    quit()
-
-allspice.log.info(f'API-Token belongs to user: {username}')
+# read
 
 
+# server is new, local is old, want local-old
+# server is new, local is old, want server-new
+# server is old, local is new, want server old
+
+
+# server    local   want
+# old       new     local->server
+# old       new     server->local
+# new       old     server->local
+# new       new     merge conflict?
