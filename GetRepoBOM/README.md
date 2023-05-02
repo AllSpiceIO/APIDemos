c3040b57df223213e22ec6074e20295cc0a92e10
Daniel Lindmark <daniel@allspice.io>
2023-05-01 21:20:41 -0500

    Created fork of GetRepoJSON to GetRepoBOM

---
+++ GetRepoBOM/README.md
@@ -0,0 +1,23 @@
+# GetRepoBOM.py
+
+## Summary
+returns a BOM from a repo
+
+## Usage
+From the command line, create two environmental variables
+This example uses bash
+
+```
+export ALLSPICE_URL="https://hub.allspice.io"
+
+export ALLSPICE_ACCESS_TOKEN="your-access-token"
+
+```
+
+To run:
+```
+python3 getRepoJSON.py > youroutfile.json
+
+```
+
+If you need help, check out our tutorial on [how to create an AllSpice access token](https://allspice.document360.io/docs/how-to-create-an-allspice-authentication-application-access-token).
---
+++ GetRepoBOM/getRepoBOM.py
@@ -0,0 +1,50 @@
+# getRepoBOM.py
+
+# This function will be replaced by built-in rate limiting
+# https://github.com/AllSpiceIO/py-allspice/issues/6
+import os
+import json
+from gitea import *
+from time import sleep
+
+# Will remove when rate limiting is added to py-allspice
+# https://github.com/AllSpiceIO/py-allspice/issues/6
+
+
+def delay_server():
+    sleep(0.01)
+
+
+try:
+    URL = os.environ['ALLSPICE_URL']
+except KeyError:
+    print("Invalid URL. Run this command:")
+    print("export ALLSPICE_URL=\"https://hub.allspice.io\"")
+    quit()
+
+try:
+    TOKEN = os.environ['ALLSPICE_ACCESS_TOKEN']
+except KeyError:
+    print("Invalid token. Set environmental variables")
+    print(">export ALLSPICE_ACCESS_TOKEN=\"YourAccessToken\"")
+    quit()
+
+allspice = Gitea(URL, TOKEN)
+delay_server()
+
+# Replace with your owner, repo, and filename
+# /repos/repo_owner/repo_name/allspice_generated/json/filename
+file_url = "/repos/AllSpiceUser/ArchimajorFork/allspice_generated/json/Mosfets.SchDoc"
+
+# Set branch / ref
+params = {"ref": "main"}
+
+file_dict = allspice.requests_get(file_url, params)
+
+# Example how to use dict
+# for key in file_dict:
+#     # print (f"{key}:{file_dict[key]}")
+
+# Convert to JSON
+file_json = json.dumps(file_dict)
+print(file_json)
